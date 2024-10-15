6e940bebe0878e68356b3e2d5a5053e544f49496
Lomlomm <vanessaserna60@gmail.com>
2024-10-15 12:19:13 -0600

    Dating app

---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+using API.DTOs;
+using API.Entities;
+using API.Helpers;
+
+namespace API.Interfaces;
+
+public interface IUserRepository
+{
+    Task<MemberDto> GetMemberAsync(string username);
+    Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams);
+    Task<AppUser> GetUserByIdAsync(int id);
+    Task<AppUser> GetUserByUsernameAsync(string username);
+    Task<IEnumerable<AppUser>> GetUsersAsync();
+    Task<bool> SaveAllAsync();
+    void Update(AppUser user);
+}
---
+++ SPA/src/app/_modules/shared.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrModule } from 'ngx-toastr';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { FileUploadModule } from 'ng2-file-upload';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { TimeagoModule } from "ngx-timeago";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: false,
+      progressBar: true
+    }),
+    NgxSpinnerModule.forRoot({
+      type: 'ball-scale-multiple'
+    }),
+    FileUploadModule,
+    BsDatepickerModule.forRoot(),
+    PaginationModule.forRoot(),
+    ButtonsModule.forRoot(),
+    TimeagoModule.forRoot(),
+  ],
+  exports: [
+    BsDropdownModule,
+    ToastrModule,
+    TabsModule,
+    NgxSpinnerModule,
+    FileUploadModule,
+    BsDatepickerModule,
+    PaginationModule,
+    ButtonsModule,
+    TimeagoModule,
+  ]
+})
+export class SharedModule { }
